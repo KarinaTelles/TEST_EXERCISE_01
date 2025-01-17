14781f5281941001a8977af015fc1cfa82e9ffa9
= <karinac.telles@gmail.com>
2025-01-17 09:50:15 -0300

    lista ordenada

--- src/main/java/com/example/exercise/Faturamento.java
+++ src/main/java/com/example/exercise/Faturamento.java
@@ -5,9 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Faturamento {
-    /*Primeiro passo: Ler um arquivo JSON*/
+    /* Primeiro passo: Ler um arquivo JSON */
     public void jsonReader() throws IOException {
         ObjectMapper objectMapperSize = new ObjectMapper();
         JsonNode jsonNodeSize = objectMapperSize.readTree(new File("src/main/java/com/example/exercise/dados.json"));
@@ -15,15 +18,29 @@ public class Faturamento {
         System.out.println("IMPRIME O TAMANHO DO JSON: " + jsonNodeSize.size());
         System.out.println("VERIFICA SE É UM ARRAY: " + jsonNodeSize.isArray());
 
-        for(int i = 0; i<jsonNodeSize.size()-1;i++){
-            ArrayList<Integer> list = new ArrayList<>();
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(new File("src/main/java/com/example/exercise/dados.json"));
-            JsonNode pegaJsonNode = jsonNode.get(i);
-            int dia = pegaJsonNode.get("dia").asInt();
-            list.add(dia);
-            System.out.println(list);
+        Map<Integer, Double> listaJsonToHashMap = jsonToHashMap(jsonNodeSize);
+        System.out.println("Mapa original: " + listaJsonToHashMap);
+
+        // Ordenar o mapa por chave (dia)
+        List<Map.Entry<Integer, Double>> listaOrdenada = new ArrayList<>(listaJsonToHashMap.entrySet());
+        listaOrdenada.sort(Map.Entry.comparingByValue());
+
+        // Imprimir a lista ordenada
+        System.out.println("Mapa ordenado: ");
+        for (Map.Entry<Integer, Double> entry : listaOrdenada) {
+            System.out.println("Dia: " + entry.getKey() + ", Valor: " + entry.getValue());
         }
+    }
+
+    public Map<Integer, Double> jsonToHashMap(JsonNode jsonNodeSize) throws IOException {
+        Map<Integer, Double> mapa = new HashMap<>();
+        for (int i = 0; i < jsonNodeSize.size(); i++) {
+            JsonNode pegaJsonNode = jsonNodeSize.get(i);
+            int dia = pegaJsonNode.get("dia").asInt();
+            double valor = pegaJsonNode.get("valor").asDouble();
+            mapa.put(dia, valor);
+        }
+        return mapa;
     }
 }
 /*
