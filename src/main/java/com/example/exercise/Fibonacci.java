a45cf06ce1432bccb4c991caff29336c562a9f4e
= <karinac.telles@gmail.com>
2025-01-17 13:28:44 -0300

    trocando os nomes em inglês pq desiste de deixar padão english

--- src/main/java/com/example/exercise/Fibonacci.java
+++ src/main/java/com/example/exercise/Fibonacci.java
@@ -5,11 +5,11 @@ public class Fibonacci {
         int valorLimite = 10;
         System.out.print("SEQUÊNCIA DE FIBONACCI COM "+ valorLimite +" NÚMEROS: ");
         for(int valorAtual = 0; valorAtual<valorLimite; valorAtual++){
-            System.out.print(doFibonacci(valorAtual) + " ");
+            System.out.print(fazFibonacci(valorAtual) + " ");
         }
         System.out.println("\n");
     }
-    public int doFibonacci(int n){
+    public int fazFibonacci(int n){
         int a = 0; int b = 1; int aux = 0;
         /*Os primeiros resultados são 1.
         Sequência: 1, 1, 2(1+1),3(1+2),5(3+2),8(5+3) etc.*/
