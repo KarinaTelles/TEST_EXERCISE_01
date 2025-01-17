c0d5058630c83db6d4ce88212c3484a902a98da3
= <karinac.telles@gmail.com>
2025-01-17 13:25:28 -0300

    thinking about it

--- src/main/java/com/example/exercise/ExerciseApplication.java
+++ src/main/java/com/example/exercise/ExerciseApplication.java
@@ -28,10 +28,4 @@ public class ExerciseApplication {
 		/*##########################################*/
 	}
 }
-/*
-5) Escreva um programa que inverta os caracteres de um string.
 
-IMPORTANTE:
-a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
-b) Evite usar funções prontas, como, por exemplo, reverse;
-* */
